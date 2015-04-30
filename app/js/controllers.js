6c2d6420b32abf9afad262a1642440dd33b19efb
towersxu <987427795@qq.com>
2015-04-30 12:02:46 +0800

    表单提交

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,22 +4,30 @@
 
 var demoControllers = angular.module('demoControllers', []);
 
-demoControllers.controller('HomeCtrl', ['$scope', '$cookies',
-  function ($scope) {
-    $scope.orderProp = 'login';
+demoControllers.controller('NavCtrl', ['$scope', '$cookieStore',
+  function ($scope, $cookieStore) {
+    $scope.userinfo = $cookieStore.get('userinfo');
+    console.log($scope.username)
   }]);
 
+demoControllers.controller('HomeCtrl', ['$scope', '$cookies', '$cookieStore',
+  function ($scope, $cookies, $cookieStore) {
+    $scope.userinfo = $cookieStore.get('userinfo');
+    console.log($scope.userinfo);
+    $scope.user = $cookies.userinfo;
+    console.log($scope.user);
+  }]);
 demoControllers.controller('LoginCtrl', ['$scope', '$http', '$cookieStore',
   function ($scope, $http, $cookieStore) {
     /*设置登陆提示信息*/
     $scope.tipInfoObject = {
-      "N":"",
-      "UE":"帐号或者密码不正确！",
-      "VE":"验证码错误！",
-      "NE":"网络出错！"
+      "N": "",
+      "UE": "帐号或者密码不正确！",
+      "VE": "验证码错误！",
+      "NE": "网络出错！"
     };
     $scope.tipInfo = $scope.tipInfoObject[$cookieStore.get('errorType')];
-    $scope.clearTipInfo = function(){
+    $scope.clearTipInfo = function () {
       $scope.tipInfo = "";
     };
     /*记住帐号和密码功能。*/
@@ -32,8 +40,22 @@ demoControllers.controller('LoginCtrl', ['$scope', '$http', '$cookieStore',
         userInfo.password = $scope.userinfo.password;
       }
       $cookieStore.put('userinfo', userInfo);
-      $cookieStore.put('errorType', "NE");
-      window.location.reload();
+      //$cookieStore.put('errorType', "NE");
+      //window.location.href = "#home";
+      $http({
+        method:'POST',
+        url:'/SSOServer/server/login',
+        data:{
+          "userAccount":$scope.userinfo.username,
+          "userPwd" : $scope.userinfo.password,
+          "url":"http://www.a.com"
+        }
+      }).success(function(data){
+        console.log("login msg ===================》");
+        console.log(data);
+        console.log("《===========================");
+        $scope.tipInfo = $scope.tipInfoObject[data];
+      });
     };
     /*设在验证码*/
     $http.get('data/verify.json').success(function (data) {
