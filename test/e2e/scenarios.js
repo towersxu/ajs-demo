a6120cf1485c01d0fe55ff3ebd0f08758e59436a
towersxu <987427795@qq.com>
2015-06-02 09:56:51 +0800

    e2e test demo

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -5,14 +5,14 @@
 describe('ajs-demo App', function() {
 
   it('should redirect index.html to index.html#/home', function() {
-    //browser.get('app/index.html');
-    //browser.getLocationAbsUrl().then(function(url) {
-    //    expect(url.split('#')[1]).toBe('/home');
-    //  });
+    browser.get('app/index.html');
+    browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/home');
+      });
   });
 
   it('should hava a title',function(){
-    //browser.get('app/index.html');
-    //expect(browser.getTitle()).toEqual('Angular practice');
+    browser.get('app/index.html');
+    expect(browser.getTitle()).toEqual('Angular practice');
   })
 });
